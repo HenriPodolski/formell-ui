10f66088a348bf0e643417b8d215964014e69c7c
Henri Podolski <henrip@gmail.com>
2015-08-11 13:40:12 +0200

    adds webdriver stack

--- test/spec/e2e/formell.spec.js
+++ test/spec/e2e/formell.spec.js
@@ -1,16 +1,16 @@
 var assert = require('assert');
+var testDoc = 'file:///'+ __dirname +'/../helpers/html/test.html';
 
 describe('grunt-webdriverjs test', function () {
 
 	it('checks if title contains the search query', function(done) {
 
 		browser
-			.url('http://webdriverjs.christian-bromann.com/')
+			.url(testDoc)
 			.getTitle(function(err,title) {
-				assert.strictEqual(title,'WebdriverJS Testpage');
+				assert.strictEqual(title,'Document');
 			})
 			.call(done);
-
 	});
 
 });
